4ff450f1764d1aa19dba3ca2de66b9a983061a51
harshyadav756 <hy200610@gmail.com>
2024-12-02 14:29:30 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,64 @@
+
+
+var myinputbox = document.getElementbyid("inputbox")
+function input1(){
+    document.getElementById("inputbox").value +="1"
+}
+function input2(){
+    document.getElementById("inputbox") .value +="2"
+}
+function input3(){
+    document.getElementById("inputbox").value +="3"
+}
+function input4(){
+    document.getElementById("inputbox").value +="4"
+}
+function input5(){
+    document.getElementById("inputbox").value +="5"
+}
+function input6(){
+    document.getElementById("inputbox").value +="6"
+}
+function input7(){
+     document.getElementById("inputbox").value +="7"
+}
+function input8(){
+     document.getElementById("inputbox").value +="8"
+}
+function input9(){
+     document.getElementById("inputbox").value +="9"
+}
+function input0(){
+     document.getElementById("inputbox").value +="0"
+}
+function input (){
+ document.getElementById("inputbox").value +=""
+}
+function input (){
+ document.getElementById("inputbox").value +="6"
+}
+function input (){
+     document.getElementById("inputbox").value +="6"
+}
+function input (){
+ document.getElementById("inputbox").value +="6"
+}
+function input (){
+    document.getElementById("inputbox") .value +="6"
+}
+function input (){
+    document.getElementById("inputbox") .value +="6"
+}
+function input (){
+    document.getElementById("inputbox") .value +="6"
+}
+function input (){
+    document.getElementById("inputbox") .value ="6"
+}
+function input6() {
+   document.getElementById("inputbox").value ="6"
+}
+function input (){
+    document.getElementById("inputbox").value ="6"
+}
+
